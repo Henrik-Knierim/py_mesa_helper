1c6fc5a027fb5f794314c887926d0807bc7b0d5a
Henrik-Knierim <henrik.knierim@uzh.ch>
2023-05-22 16:34:54 +0000

    typo correction

--- mesa_inlist_manager/Analysis.py
+++ mesa_inlist_manager/Analysis.py
@@ -4,11 +4,11 @@ import mesa_reader as mr
 
 class Analysis:
 
-    def __init__(self, src = './LOGS/') -> None:
+    def __init__(self, src = 'LOGS') -> None:
         # location of the LOGS directory
         self.src = src
     
-    def homogeinity(self, model_number = -1):
+    def homogeneity(self, model_number = -1):
         """Gives the deviation of the metallicity from a homogeneous profile."""
 
         # init log object
@@ -17,7 +17,10 @@ class Analysis:
         profile = log.profile_data(model_number = model_number)
 
         # get average metallicty
-        Z_avg = log.history.data('average o16')[model_number] # check definition
+        if model_number == -1:
+            Z_avg = log.history.data('average_o16')[-1]
+        else:
+            Z_avg = log.history.data_at_model_number('average_o16', m_num = model_number) # check definition
 
         # get the metallicity profile
         dm = profile.dm
@@ -28,19 +31,19 @@ class Analysis:
 
         return np.sqrt(variance)
     
-    def relative_atmospheric_metallicity(self):
-        """Gives the ratio between the atmopsheric metallicity (Z_atm) and the average metallicity (Z_avg)."""
+    # def relative_atmospheric_metallicity(self):
+    #     """Gives the ratio between the atmopsheric metallicity (Z_atm) and the average metallicity (Z_avg)."""
 
-        # init log object
-        log = mr.MesaLogDir(self.src)
+    #     # init log object
+    #     log = mr.MesaLogDir(self.src)
         
-        # gets the last profile
-        profile = log.profile_data()
+    #     # gets the last profile
+    #     profile = log.profile_data()
 
-        # get the metallicity profile
-        dm = profile.dm
-        Z = profile.z
+    #     # get the metallicity profile
+    #     dm = profile.dm
+    #     Z = profile.z
 
-        # calculate the relative atmospheric metallicity
-        return np.sum(Z * dm) / np.sum(dm)
+    #     # calculate the relative atmospheric metallicity
+    #     return Z_atm/Z_avg
         
